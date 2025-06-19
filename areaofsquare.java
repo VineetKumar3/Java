fe3e2b94da653e75d99a595cd7dd00ef384a22e5
Fusionop3 <vineetkumar2823@outlook.com>
2025-06-19 16:42:59 +0530

    new

---
+++ areaofsquare.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class areaofsquare {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print(" Enter the sides of the square: ");
+        int side = scan.nextInt();
+        double area = side*side;
+
+        System.out.println("The area of the square is:"+ area+"Square units");
+
+        int $ = 1;
+        System.out.print($);
+    }
+}
