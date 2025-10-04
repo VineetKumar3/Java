bf8989380a802fdf131073d06ed1dee1e5b8d308
Fusionop3 <vineetkumar2823@outlook.com>
2025-10-04 18:49:33 +0530

    after gitignore file is addeed

--- methods.java
+++ methods.java
@@ -2,7 +2,7 @@ public class methods {
     public static void main(String args[]){
         String name = "John";
         System.out.println(name.length());
-
+        System.out.println(name.charAt(2));        
     }
 
 }
