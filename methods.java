1fe789b4eb8843f74b12efde3d42707c27e06231
Fusionop3 <vineetkumar2823@outlook.com>
2025-10-25 18:38:52 +0530

    new

--- methods.java
+++ methods.java
@@ -1,8 +1,11 @@
 public class methods {
     public static void main(String args[]){
-        String name = "John";
+        String name = "John wick";
         System.out.println(name.length());
-        System.out.println(name.charAt(2));        
+        System.out.println(name.charAt(2));
+        System.out.println(name.trim());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
     }
 
 }
